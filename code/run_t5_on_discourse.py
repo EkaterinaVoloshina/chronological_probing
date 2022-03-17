3be2b9a7d012d782a83ff14b8ec35ae5bdc42d30
Ekaterina Voloshina <42929264+EkaterinaVoloshina@users.noreply.github.com>
2022-03-17 14:13:41 +0300

    Create run_t5_on_discourse.py

---
+++ code/run_t5_on_discourse.py
@@ -0,0 +1,25 @@
+from utils import load_files
+from tqdm.notebook import tqdm
+from t5_discourse_embeddings import Embeddings
+
+
+def get_checkpoints(num):
+    checkpoints = num
+    path = []
+    for i in range(checkpoints):
+        num = i * 100000
+        link = f'../../small_wiki_bs_128/model_{num}.pth'  # path to a model
+        path.append(link)
+    return checkpoints, path
+        
+def main():
+    task_name = 'Conn'  # change the filename
+    X_train, y_train, X_test, y_test = load_files(task_name)
+    checkpoints, path = get_checkpoints(12)
+    embeddings = Embeddings(path, X_train, checkpoints, 512, f'{task_name}_TRAIN', 'conn')
+    metrics = embeddings.calculate()
+    embeddings = Embeddings(path, X_test, checkpoints, 512, f'{task_name}_TEST', 'conn')
+    embeddings.calculate()
+
+if __name__ == "__main__":
+    main()
